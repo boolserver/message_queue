b7ec309098733b015e6c77953471d9fac5b70c8e
Anmol1696 <anmol1696@gmail.com>
2017-09-01 14:13:18 +0530

    Making main into functions

--- msg_queue.c
+++ msg_queue.c
@@ -1,18 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <mqueue.h>
-#include <unistd.h>
-#include <assert.h>
-
-#define BUF_SIZE 256
+#include "msg_queue.h"
 
 unsigned char *buffer_genration(const char *filename){
     FILE *fp = fopen(filename, "rb");
-    //unsigned char buff[BUF_SIZE] = {0};
     unsigned char *buff = malloc(sizeof(char)*BUF_SIZE);
-
+    
     fread(buff, 1, BUF_SIZE, fp);
     fclose(fp);
 
@@ -23,72 +14,91 @@ mqd_t init_message_queue(){
     struct mq_attr m_attribute;
     m_attribute.mq_flags = 0;
     m_attribute.mq_maxmsg = 100;
-    m_attribute.mq_msgsize = BUF_SIZE;
-    //m_attribute.mq_curmsgs = 0;
+    m_attribute.mq_msgsize = UUID_SIZE_FOR_STR + 1;
+    m_attribute.mq_curmsgs = 0;
     
-    mqd_t m_queue = mq_open("/tmp_queue", O_RDWR | O_CREAT | O_NONBLOCK, 0666, &m_attribute);
+    mqd_t m_queue = mq_open(QUEUE_MOUNT, O_RDWR | O_CREAT | O_NONBLOCK, 0666, &m_attribute);
 
     if(m_queue == -1){printf("ERROR in initializing\n");exit(100);}
 
     printf("Initiated Message Queue\n");
-
+    
     return m_queue;
+}
+
+void uuid_to_str(uuid_t uuid, char* str_ptr){
+    sprintf(
+        str_ptr, "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x", 
+        uuid[0], uuid[1], uuid[2], uuid[3], uuid[4], uuid[5], uuid[6], uuid[7],
+        uuid[8], uuid[9], uuid[10], uuid[11], uuid[12], uuid[13], uuid[14], uuid[15]
+    );
+}
+
+void send_m_buffer_to_queue(char *m_buffer){
+    mqd_t m_queue_2 = mq_open(QUEUE_MOUNT, O_WRONLY | O_NONBLOCK);
+    //char* temp_buf = m_buffer->uuid_str;
+    printf("\tTemp buf -> %s and size -> %d\n", m_buffer, sizeof(m_buffer));
+    int status = mq_send(m_queue_2, m_buffer, UUID_SIZE_FOR_STR, 1);
+    if(status == -1){printf("Failed to send data to m queue\n");}
+}
+
+char* receive_m_buffer_from_queue(){
+    //printf("Size of r buffer -> %s\n", sizeof(r_buffer));
+    mqd_t m_queue_3 = mq_open(QUEUE_MOUNT, O_RDONLY | O_NONBLOCK);
+    char* r_buffer = malloc(UUID_SIZE_FOR_STR + 1);
+    int status = mq_receive(m_queue_3, r_buffer, UUID_SIZE_FOR_STR+1, NULL);
+    if(status == -1){printf("Failed to RECEIVE data from m queue\n");}
+    printf("Recived -> %s\n", r_buffer);
+    //r_buffer->uuid_str = &temp_buf;
+    return r_buffer;
 }
 
 int main(int argc, char **argv){
     printf("Starting code\n");
     int status = 0;
-    if(argc == 2){
-        const char *filename = argv[1];
-    } 
-    else{
-        printf("ERROR in filename defination\n");
-    }
-    printf("HERE\n");
-    char* buffer = buffer_genration("test_file.json");
-    size_t buf_len = strlen(buffer);
-   
     printf("Removing previous copy of the message queue\n");
-    status = mq_unlink("/tmp_queue");
+    status = mq_unlink(QUEUE_MOUNT);
     //ASSERT(status != 0);
     printf("Status Unlink -> %d\n", status);
 
     printf("Starting intialization of message queue\n");
     mqd_t m_queue = init_message_queue();
-
-    //testing m_queue
-    printf("Sending data\n"); 
-    size_t temp = 6*sizeof(char);
-    temp = 6;
-
-    printf("Buffer -> %s\n", buffer);
-
-    status = mq_send(m_queue,buffer, buf_len, 1);
-    printf("Status Sending = %d\n", status);
-
-    sleep(1);
     
-    //mqd_t m_queue_2 = mq_open("/tmp_queue3", O_RDONLY | O_NONBLOCK);
-
     struct mq_attr m_attribute;
 
     mq_getattr(m_queue, &m_attribute);
 
     printf("Flags -> %d, mq_maxmsg -> %d, mq_maxsize -> %d, mq_curmsgs -> %d\n", m_attribute.mq_flags, m_attribute.mq_maxmsg, m_attribute.mq_msgsize, m_attribute.mq_curmsgs);
+    
+    //struct msgForQueue temp_msg;
+    //struct msgForQueue* msg_ptr;
 
-    char *buffer_2 = malloc(BUF_SIZE);
-    //char buffer_2[sizeof(char) * 6];
-    printf("Starting the receving functions\n");
-    status = mq_receive(m_queue, buffer_2, BUF_SIZE, NULL);
-    printf("Status Recive = %d\n", status);
+    //struct msgForQueue r_msg;
+    //struct msgForQueue* r_ptr;
 
-    //printf("Buffer -> %s\n", buffer_2);
+    //r_ptr = &r_msg;
 
-    //Testing *buffer functions
-    FILE *fp = fopen("test_run_s2.json", "wb");
-    fwrite(buffer_2, 1, strlen(buffer_2), fp);
-    fclose(fp);
+    //msg_ptr = &temp_msg;
 
-    //mq_unlink("/test_queue")
+    uuid_t uuid;
+    uuid_generate_random(uuid);
+    
+    unsigned char* temp_msg = malloc(UUID_SIZE_FOR_STR);
+    //char* r_msg = malloc(UUID_SIZE_FOR_STR);
+
+    uuid_to_str(uuid, temp_msg);
+
+    printf("UUID in temp_msg -> %s\nSending Msg\n", temp_msg);
+
+    send_m_buffer_to_queue(temp_msg);
+    printf("Msg sent\nReciving msg\n");
+
+    mq_getattr(m_queue, &m_attribute);
+
+    printf("Flags -> %d, mq_maxmsg -> %d, mq_maxsize -> %d, mq_curmsgs -> %d\n", m_attribute.mq_flags, m_attribute.mq_maxmsg, m_attribute.mq_msgsize, m_attribute.mq_curmsgs);
+    char* r_msg = receive_m_buffer_from_queue(r_msg);
+    printf("Size of r_msg -> %d\n", sizeof(r_msg)); 
+    printf("UUID recived buffer -> %s\n", r_msg);
+
     return 0;
 }
