d7aac4011044e052e47b6c7f9b2275f05f14af47
Anmol1696 <anmol1696@gmail.com>
2017-09-02 01:56:06 +0530

    Header file for client side in msg_queue

---
+++ client_from_frontend.h
@@ -0,0 +1,11 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "msg_queue.h"
+
+#define SERVER_IP_ADDR "127.0.0.1"
+#define PORT 5000
+
+int client(const char* uuid_str);
+
