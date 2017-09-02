60da60faafa61833cf88d25568b63b83d42cbb47
Anmol1696 <anmol1696@gmail.com>
2017-09-02 14:58:55 +0530

    Client side code for uuid transfer from frontend to msg queue

--- client_from_frontend.c
+++ client_from_frontend.c
@@ -4,17 +4,17 @@ int init_sockfd(){
     int sockfd = 0;
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){ 
         printf("\nERROR: Could not create socket\n");
-        exit(1);
+        return -1;
     }
     
     struct sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-    serv_addr.sin_addr.s_addr = inet_addr(SERVER_IP_ADDR);
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
-        printf("\nERROR : Connect Failed\n");
-        exit(1);
+        printf("\nERROR : Connect Failed\n\n");
+        return -1;
     }
 
     return sockfd;
@@ -22,13 +22,20 @@ int init_sockfd(){
 
 char* get_uuid_as_client(){
     int sockfd = init_sockfd();
-
+    printf("Initialized socket for client\n");
     char* uuid_buf = malloc(UUID_SIZE_FOR_STR);  // UUID str will be stored in temp_buf
     memset(uuid_buf, '0', sizeof(uuid_buf));
-    int bytes_received = read(sockfd, uuid_buf, UUID_SIZE_FOR_SIZE);
-
+    int bytes_received = read(sockfd, uuid_buf, UUID_SIZE_FOR_STR);
+    printf("Bytes recived -> %i\n", bytes_received);
     if(bytes_received < UUID_SIZE_FOR_STR){printf("\nERROR: bytes read less then UUID size\n");}
 
-    return uuid_buf
+    return uuid_buf;
 }
 
+int main(){
+    char* r_uuid;
+    printf("Starting the Client\n");
+    r_uuid = get_uuid_as_client();
+
+    printf("Recived this UUID-> %s\n", r_uuid);
+}
