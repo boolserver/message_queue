40d4adb5e78aa42634c111cbd992c3019fcca12f
Anmol1696 <anmol1696@gmail.com>
2017-09-02 15:27:24 +0530

    Converting client to server

--- server_for_frontend.c
+++ server_for_frontend.c
@@ -1,31 +1,28 @@
-#include "client_from_frontend.h"
+#include "server_for_frontend.h"
 
-int init_sockfd(){ 
-    int sockfd = 0;
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){ 
-        printf("\nERROR: Could not create socket\n");
-        return -1;
-    }
-    
+int init_socket(){
+    int listenfd = socket(AF_INET, SOCK_STREAM, 0); 
+
     struct sockaddr_in serv_addr;
+    memset(&serv_addr, '0', sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(PORT);
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
-        printf("\nERROR : Connect Failed\n\n");
-        return -1;
-    }
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+                                    
+    if(listen(listenfd, 10) == -1){printf("\nERROR: Failed to listen\n"); return -1;}
 
-    return sockfd;
+    return listenfd;
 }
 
 char* get_uuid_as_client(){
-    int sockfd = init_sockfd();
+    int listenfd = init_socket();
     printf("Initialized socket for client\n");
+    int connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
     char* uuid_buf = malloc(UUID_SIZE_FOR_STR);  // UUID str will be stored in temp_buf
     memset(uuid_buf, '0', sizeof(uuid_buf));
-    int bytes_received = read(sockfd, uuid_buf, UUID_SIZE_FOR_STR);
+    int bytes_received = read(connfd, uuid_buf, UUID_SIZE_FOR_STR);
     printf("Bytes recived -> %i\n", bytes_received);
     if(bytes_received < UUID_SIZE_FOR_STR){printf("\nERROR: bytes read less then UUID size\n");}
 
