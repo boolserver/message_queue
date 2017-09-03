a2f97ffda04878eca2f8f208906312446bb417b9
Anmol1696 <anmol1696@gmail.com>
2017-09-03 14:31:07 +0530

    Incorporated msgs from msg queue as well

--- server_for_frontend.c
+++ server_for_frontend.c
@@ -16,12 +16,13 @@ int init_socket(){
     return listenfd;
 }
 
-char* get_uuid_as_client(){
-    int listenfd = init_socket();
-    printf("Initialized socket for client\n");
-    int connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+char* get_uuid_as_server(int connfd){
+    //int listenfd = init_socket();
+    //printf("Initialized socket for client\n");
+    //int connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
     char* uuid_buf = malloc(UUID_SIZE_FOR_STR);  // UUID str will be stored in temp_buf
     memset(uuid_buf, '0', sizeof(uuid_buf));
+    //printf("Waiting for client now\n");
     int bytes_received = read(connfd, uuid_buf, UUID_SIZE_FOR_STR);
     printf("Bytes recived -> %i\n", bytes_received);
     if(bytes_received < UUID_SIZE_FOR_STR){printf("\nERROR: bytes read less then UUID size\n");}
@@ -31,8 +32,25 @@ char* get_uuid_as_client(){
 
 int main(){
     char* r_uuid;
-    printf("Starting the Client\n");
-    r_uuid = get_uuid_as_client();
+    printf("Starting the Client\n"); 
+    int listenfd = init_socket();
+    char frontend_or_msgqueue;
+    
+    while(1){
+        int connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+        int br = read(connfd, &frontend_or_msgqueue, 1);
+        if(br != 1){printf("\nInitiation not defined\n\n");}
 
-    printf("Recived this UUID-> %s\n", r_uuid);
+        if(frontend_or_msgqueue == 'f'){
+            r_uuid = get_uuid_as_server(connfd);
+            printf("Pushing data to msg queue");
+            send_m_buffer_to_queue(r_uuid);
+            printf("Recived this UUID-> %s\n", r_uuid);
+        }
+        else if(frontend_or_msgqueue == 'm'){
+          char* r_uuid_str = receive_m_buffer_from_queue();
+          write(connfd, r_uuid_str, UUID_SIZE_FOR_STR);
+        }
+        close(connfd);
+    }
 }
